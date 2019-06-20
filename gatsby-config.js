957142ecf15476e9a41f39c5f9ca94aa58540994
Aokashi <Aokashi.Towa@Gmail.com>
2019-06-20 12:45:38 +0900

    サイトのタイトルと説明、ナビゲーション項目を変更

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Gatsby + Netlify のテストサイト',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'これは Gatsby と Netlify CMS で構築されたビジネス用のWebサイトのサンプルです。 JAM スタックアーキテクチャに準拠していて、 Git リポジトリの使用や Netlify による継続的デプロイ、 CDN による配信によって成り立っています。',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,17 +63,17 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
+              <Link className="navbar-item" to="/download">
+                Download
+              </Link>
+              <Link className="navbar-item" to="/phoenix">
+                Phoenix
               </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
               <Link className="navbar-item" to="/contact">
                 Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
